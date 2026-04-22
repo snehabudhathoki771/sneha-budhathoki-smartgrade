5a54737fb754091ea3b580d08b4c2e269197cf17
snehabudhathoki771 <snehabudhathoki78@gmail.com>
2026-04-22 10:43:56 +0545

    Updated for redeploying

--- Backend/SmartGrade/Program.cs
+++ Backend/SmartGrade/Program.cs
@@ -154,7 +154,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //db.Database.Migrate();
+    db.Database.Migrate();
 }
 
 app.Run();
